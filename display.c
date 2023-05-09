851a7337398ba1055e6aebd0524bc8eddbe0289a
AlishaNaraC <117899918+AlishaNaraC@users.noreply.github.com>
2023-05-09 23:34:11 +0700

    memperbaiki DisplayMenuAwal()

--- display.c
+++ display.c
@@ -1,3 +1,16 @@
+/*  File        : display.c 
+    Deskripsi   : Display untuk program antrian pelanggan di kassa
+    Identitas   : Alisha Nara Chandrakirana     (221524033)
+                  Keanu Rayhan Harits           (221524043)
+                  Niqa Nabila Nur Ihsani        (221524054)
+    Kelompok    : 2C
+    Kelas       : 1B-D4IT
+    Mata Kuliah : Struktur Data dan Algoritma
+    Tugas       : Tugas Besar membuat program antrian pelanggan pada kassa di minimarket
+    Tanggal     : 27 Maret 2023 s.d. 7 Mei 2023
+                  Update 9 Mei 2023
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -15,104 +28,111 @@ void warnateks(int warna) //modul yang berfungsi untuk memberi warna ke karakter
 	SetConsoleTextAttribute(hConsole, warna);
 }
 
-void gotoxy(int x, int y) {
-    COORD coord;
+// void gotoxy(int x, int y) {
+//     COORD coord;
      
-    coord.X = x;
-    coord.Y = y;
+//     coord.X = x;
+//     coord.Y = y;
      
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
+//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+// }
 
-void DisplayLoading(){
-	int bg, fg, delay, panjang;
-	int simbol[20] = {2,3,4,5,6,14,16,22,35,36,62,64,158,174,175,176,177,178,219};
-	warnateks(LIGHT_BLUE);
-   	loading(20 - 1, 19 - 1, 30 , 1, simbol);
-   	gotoxy(48,10);
-}
+// void DisplayLoading(){
+// 	int bg, fg, delay, panjang;
+// 	int simbol[20] = {2,3,4,5,6,14,16,22,35,36,62,64,158,174,175,176,177,178,219};
+// 	warnateks(LIGHT_BLUE);
+//    	loading(20 - 1, 19 - 1, 30 , 1, simbol);
+//    	gotoxy(48,10);
+// }
 
-void loading(int bg, int fg, int panjang, int delay, int simbol[20]){
- int k, l;
-   for (k=0; k<=panjang; k+=2)
-   {
-      gotoxy(48,10);
-      printf("\n\n\n\n\t\t\t\t\t\tPlease Wait \n\t");
-      printf("\n\t\t\t\t\t");
-      for (l=0; l<k; l++){
-         printf("%c", simbol[fg]);
-      }
-      printf("\t\t\t\t\t\t");
-      for (l=0; l<panjang-1; l++){
-         printf("%c", simbol[bg]);
-      Sleep(delay);
-      }
-   }
-}
+// void loading(int bg, int fg, int panjang, int delay, int simbol[20]){
+//  int k, l;
+//    for (k=0; k<=panjang; k+=2)
+//    {
+//       gotoxy(48,10);
+//       printf("\n\n\n\n\t\t\t\t\t\tPlease Wait \n\t");
+//       printf("\n\t\t\t\t\t");
+//       for (l=0; l<k; l++){
+//          printf("%c", simbol[fg]);
+//       }
+//       printf("\t\t\t\t\t\t");
+//       for (l=0; l<panjang-1; l++){
+//          printf("%c", simbol[bg]);
+//       Sleep(delay);
+//       }
+//    }
+// }
 
 void DisplayMenuAwal(){
-    // system("cls");
-    warnateks(LIGHT_BLUE);
-	 printf("\n\n\n\n\n\n\n"); 
-     printf("\t\tCCCCCCC   CCCCCC   CCCCCC         CCCCC CCCCC   CCCCCC   CCCCC    CCCCCCC\n");
-     printf("\t\t      C  C     C  C     C         C   CCC   C  C      C  C    C      C\n");
-     printf("\t\tCCCCCCC  C        C        CCCCC  C         C  C      C  C     C     C\n");
-     printf("\t\tC        C     C  C     C         C CCC CCC C  C CCCC C  CCCCCCC     C\n");
-     printf("\t\tCCCCCCC   CCCCCC   CCCCCC         CCC CCC CCC  CCC  CCC  C     C     C\n\n");
-     warnateks(WHITE);
-     printf("\t\t\t\t   -----------------------------------\n");
-     printf("\t\t\t\t\t   !! SELAMAT DATANG !!\n");
-     printf("\t\t\t\t\t        1. MASUK\n");
-     printf("\t\t\t\t\t        2. ATURAN\n");
-     printf("\t\t\t\t\t        0. KELUAR\n");
-     printf("\t\t\t\t   -----------------------------------\n");
-     printf("\t\t\t\t   PILIH OPSI: ");
-    //  DisplayLoading();
+    printf("\n\n\n"); 
+    printf("   =====================================================================================\n");
+    printf("  |                                                                                     |\n");
+    printf("  |      CCCCCCC   CCCCCC   CCCCCC         CCCCC CCCCC   CCCCCC   CCCCC    CCCCCCC      |\n");
+    printf("  |            C  C     C  C     C         C   CCC   C  C      C  C    C      C         |\n");
+    printf("  |      CCCCCCC  C        C        CCCCC  C         C  C      C  C     C     C         |\n");
+    printf("  |      C        C     C  C     C         C CCC CCC C  C CCCC C  CCCCCCC     C         |\n");
+    printf("  |      CCCCCCC   CCCCCC   CCCCCC         CCC CCC CCC  CCC  CCC  C     C     C         |\n");
+    printf("  |                                                                                     |\n");
+    printf("   =====================================================================================\n");
+    printf("  /     /     /     /     /     /     /     2CC     \\     \\     \\     \\     \\     \\     \\\n");
+    printf(" /____ /____ /____ /____ /____ /____ /____ -MART ____\\ ____\\ ____\\ ____\\ ____\\ ____\\ ____\\\n");
+    printf("[_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____]\n");
+    printf("     |                                                                               |\n");
+    printf("     |                         ============================                          |\n");
+    printf("     |                        |   !!  SELAMAT DATANG  !!   |                         |\n");
+    printf("     |                        |                            |                         |\n");
+    printf("     |                        |         1. MASUK           |                         |\n");
+    printf("     |                        |         2. ATURAN          |                         |\n");
+    printf("     |                        |         0. KELUAR          |                         |\n");
+    printf("     |                        |                            |                         |\n");
+    printf("     |________________________|____________________________|_________________________|\n");
+    printf("                                                                By: Alisha-Keanu-Niqa \n");
+    printf("     PILIH OPSI: ");
 }
 
 void DisplayMenuMasuk(){
     // system("cls");
     printf("\n\n\n"); 
-    printf("\t\t\t\t --------------------------------\n");
-    printf("\t\t\t\t|           2CC - MART           |\n");
-    printf("\t\t\t\t|       - SELAMAT DATANG -       |\n");
-    printf("\t\t\t\t|                                |\n");
-    printf("\t\t\t\t|  1. Tambah pelanggan           |\n");
-    printf("\t\t\t\t|  2. Tampilkan daftar pelanggan |\n");
-    printf("\t\t\t\t|  3. Lainnya                    |\n");
-    printf("\t\t\t\t|  0. Kembali                    |\n");
-    printf("\t\t\t\t --------------------------------\n");
-    printf("\t\t\t\tPilih opsi: ");
+    printf(" --------------------------------\n");
+    printf("|           2CC - MART           |\n");
+    printf("|       - SELAMAT DATANG -       |\n");
+    printf("|                                |\n");
+    printf("|  1. Tambah pelanggan           |\n");
+    printf("|  2. Tampilkan daftar pelanggan |\n");
+    printf("|  3. Lainnya                    |\n");
+    printf("|  0. Kembali                    |\n");
+    printf(" --------------------------------\n");
+    printf("Pilih opsi: ");
 }
 
 void DisplayMenuLanjutan(){     // belum dibuat alurnya
     // system("cls");
     printf("\n\n\n"); 
-    printf("\t\t\t[digunakan ketika pelanggan ingin melakukan suatu perilaku saat sedang mengantri]\n\n");
-    printf("\t\t\t\t\t -----------------------------------\n");
-    printf("\t\t\t\t\t|  1. Selesai transaksi             |\n");
-    printf("\t\t\t\t\t|  2. Keluar dari antrian tengah    |\n");
-    printf("\t\t\t\t\t|  3. Keluar dari antrian belakang  |\n");
-    printf("\t\t\t\t\t|  0. Kembali                       |\n");
-    printf("\t\t\t\t\t -----------------------------------\n");
-    printf("\t\t\t\t\tPilih opsi: ");
+    printf("[digunakan ketika pelanggan ingin melakukan suatu perilaku saat sedang mengantri]\n\n");
+    printf(" -----------------------------------\n");
+    printf("|  1. Selesai transaksi             |\n");
+    printf("|  2. Keluar dari antrian tengah    |\n");
+    printf("|  3. Keluar dari antrian belakang  |\n");
+    printf("|  0. Kembali                       |\n");
+    printf(" -----------------------------------\n");
+    printf("Pilih opsi: ");
 }
 
 void DisplayBelanja(){
     printf("\n\n\n"); 
-    printf("\t\t\t\tCCC\n");
-	printf("\t\t\t\t  CC   CCCCCCCCCCCCCCC\n");
-	printf("\t\t\t\t   CC              CC\n");
-	printf("\t\t\t\t    CC            CC\n");
-	printf("\t\t\t\t     CC          CC\n");
-	printf("\t\t\t\t      CCCCCCCCCCCC\n");
-	printf("\t\t\t\t      CC       CC\n");
-	printf("\t\t\t\t     C  C     C  C\n");
-	printf("\t\t\t\t      CC       CC\n\n");
-	printf("\t\t\t\t------------------------\n");	
-    printf("\t\t\t\t ! SELAMAT BERBELANJA ! \n");
-	printf("\t\t\t\t------------------------\n");
-    printf("\t\t\t\tTekan apapun untuk melanjutkan...");
+    printf("CCC\n");
+	printf("  CC   CCCCCCCCCCCCCCC\n");
+	printf("   CC              CC\n");
+	printf("    CC            CC\n");
+	printf("     CC          CC\n");
+	printf("      CCCCCCCCCCCC\n");
+	printf("      CC       CC\n");
+	printf("     C  C     C  C\n");
+	printf("      CC       CC\n\n");
+	printf("------------------------\n");	
+    printf(" ! SELAMAT BERBELANJA ! \n");
+	printf("------------------------\n");
+    printf("Tekan apapun untuk melanjutkan...");
     getch();
 }
 
@@ -120,60 +140,62 @@ void DisplayMenuBelanja(){
     // ambil kode customer
     // system("cls");
     printf("\n\n\n"); 
-    printf("\t\t\t\t\t\tMENU BELANJA\n");
-	printf("\t\t\t\t -------------------------------------\n");
-    printf("\t\t\t\t|   1. Tambah barang belanja          |\n");
-    printf("\t\t\t\t|   2. Ubah jumlah barang belanja     |\n");
-    printf("\t\t\t\t|   3. Mengembalikan barang belanja   |\n");
-    printf("\t\t\t\t|   4. Tampilkan keranjang belanja    |\n");
-    printf("\t\t\t\t|   5. Selesai belanja                |\n");
-	printf("\t\t\t\t -------------------------------------\n");
-    printf("\t\t\t\tPilih opsi: ");
+    printf("MENU BELANJA\n");
+	printf(" -------------------------------------\n");
+    printf("|   1. Tambah barang belanja          |\n");
+    printf("|   2. Ubah jumlah barang belanja     |\n");
+    printf("|   3. Mengembalikan barang belanja   |\n");
+    printf("|   4. Tampilkan keranjang belanja    |\n");
+    printf("|   5. Selesai belanja                |\n");
+	printf(" -------------------------------------\n");
+    printf("Pilih opsi: ");
 }
 
 void DisplayAturan(){
     // system("cls");
     printf("\n\n\n"); 
-    printf("\t\t\t\t    ! ! ! ! !  Aturan Berbelanja di 2CC-Mart  ! ! ! ! !\n");
-    printf("\t\t\t\t ---------------------------------------------------------\n");
-    printf("\t\t\t\t|    1. Pelanggan tidak boleh menyela antrian pelanggan   |\n");
-    printf("\t\t\t\t|       lain di kassa                                     |\n");
-    printf("\t\t\t\t|    2. Pelanggan yang sudah antri dan belum didepan      |\n");
-    printf("\t\t\t\t|       kasir, boleh mengubah barang belanja dengan       |\n");
-    printf("\t\t\t\t|       konsekuensi akan kembali antri dari belakang      |\n");
-    printf("\t\t\t\t ---------------------------------------------------------\n\n");
-    printf("\t\t\t\tTekan apapun untuk melanjutkan...");
+    printf("    ! ! ! ! !  Aturan Berbelanja di 2CC-Mart  ! ! ! ! !\n");
+    printf(" ---------------------------------------------------------\n");
+    printf("|    1. Pelanggan tidak boleh menyela antrian pelanggan   |\n");
+    printf("|       lain di kassa                                     |\n");
+    printf("|    2. Pelanggan yang sudah antri dan belum didepan      |\n");
+    printf("|       kasir, boleh mengubah barang belanja dengan       |\n");
+    printf("|       konsekuensi akan kembali antri dari belakang      |\n");
+    printf(" ---------------------------------------------------------\n\n");
+    printf("Tekan apapun untuk melanjutkan...");
 }
 
 void DisplayAntri(){
     // system("cls");
-    printf("\t\t     CCCC\t     CCCC\t     CCCC\n");
-    printf("\t\t   CCCCCCCC\t   CCCCCCCC\t   CCCCCCCC\n");
-    printf("\t\t   CCCCCCCC\t   CCCCCCCC\t   CCCCCCCC\n");
-    printf("\t\t    CCCCCC\t    CCCCCC\t    CCCCCC\n");
-    printf("\t\t  CCCCCCCCCC\t  CCCCCCCCCC\t  CCCCCCCCCC\n");
-    printf("\t\t CCCCCCCCCCCC\t CCCCCCCCCCCC\t CCCCCCCCCCCC\n");
-    printf("\t\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\n");
-    printf("\t\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\n\n");
-    printf("\t\t\t-------------------------------\n");
-    printf("\t\t\t    SILAKAN ANTRI DI KASSA\n");
-    printf("\t\t\t-------------------------------\n");
-    printf("\t\t\t\tTekan apapun untuk melanjutkan...");
+    printf("\n\n\n"); 
+    printf("     CCCC\t     CCCC\t     CCCC\n");
+    printf("   CCCCCCCC\t   CCCCCCCC\t   CCCCCCCC\n");
+    printf("   CCCCCCCC\t   CCCCCCCC\t   CCCCCCCC\n");
+    printf("    CCCCCC\t    CCCCCC\t    CCCCCC\n");
+    printf("  CCCCCCCCCC\t  CCCCCCCCCC\t  CCCCCCCCCC\n");
+    printf(" CCCCCCCCCCCC\t CCCCCCCCCCCC\t CCCCCCCCCCCC\n");
+    printf("CCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\n");
+    printf("CCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\tCCCCCCCCCCCCCC\n\n");
+    printf("-------------------------------\n");
+    printf("    SILAKAN ANTRI DI KASSA\n");
+    printf("-------------------------------\n");
+    printf("Tekan apapun untuk melanjutkan...");
     getch();
 }
 
 void DisplayThank(){
     // system("cls");
-    printf("\t\t 	 CCC\n");
-	printf("\t\t 	C  C\n");
-	printf("\t\t CCCC  C  CCCCCC\n");
-	printf("\t\t C  C  C        C\n");
-	printf("\t\t C  C  C        C\n");
-	printf("\t\t C  C  C        C\n");
-	printf("\t\t CCCC   CCCCCCCC\n\n");
-	printf("\t ---------------------------------\n");
-	printf("\t   TERIMA KASIH SUDAH BERBELANJA\n");
-	printf("\t    	    DI 2CC-MART\n");
-	printf("\t ---------------------------------\n");
+    printf("\n\n\n"); 
+    printf(" 	 CCC\n");
+	printf(" 	C  C\n");
+	printf(" CCCC  C  CCCCCC\n");
+	printf(" C  C  C        C\n");
+	printf(" C  C  C        C\n");
+	printf(" C  C  C        C\n");
+	printf(" CCCC   CCCCCCCC\n\n");
+	printf(" ---------------------------------\n");
+	printf("   TERIMA KASIH SUDAH BERBELANJA\n");
+	printf("    	    DI 2CC-MART\n");
+	printf(" ---------------------------------\n");
 
 }
